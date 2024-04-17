bf3d3d18d4291d2486f533ad624437eb6d4ce8fc
meta-d <meta.digital.cloud@gmail.com>
2024-04-17 18:44:00 +0800

    fix: escapeBWSlash for sapbw

--- packages/xmla/src/lib/entity.service.ts
+++ packages/xmla/src/lib/entity.service.ts
@@ -99,6 +99,7 @@ export class XmlaEntityService<T> extends AbstractEntityService<T> implements En
       return EMPTY
     }
 
+    const dialect = this.dataSource.options.dialect
     const cube = this.dataSource.options.schema?.cubes?.find(({ name }) => name === this.entitySet)
     try {
       if (options) {
@@ -135,14 +136,16 @@ export class XmlaEntityService<T> extends AbstractEntityService<T> implements En
           map(({ rows, columns, data, cellset, columnAxes }) => {
             const fields = [...mdxQuery.rows, ...mdxQuery.columns]
             // for SAP BW escaped property name _
-            fields.forEach((field) => {
-              field.properties?.forEach((property) => {
-                const escaped = escapeBWSlash(field.dimension, property)
-                data.forEach((element) => {
-                  element[property] = element[escaped]
+            if (dialect === MDXDialect.SAPBW) {
+              fields.forEach((field) => {
+                field.properties?.forEach((property) => {
+                  const escaped = escapeBWSlash(field.dimension, property)
+                  data.forEach((element) => {
+                    element[property] = element[escaped]
+                  })
                 })
               })
-            })
+            }
 
             let results = data.map((item) => {
               const _item = { ...item }
