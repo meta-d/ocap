267a93a79bb09c797ca2fde4b7e5f2b46fd2e22f
meta-d <meta.digital.cloud@gmail.com>
2024-05-10 19:40:43 +0800

    feat: sync dimension members

--- apps/cloud/src/app/features/semantic-model/model/members/members.component.ts
+++ apps/cloud/src/app/features/semantic-model/model/members/members.component.ts
@@ -1,21 +1,36 @@
 import { CommonModule } from '@angular/common'
 import { Component, inject, signal } from '@angular/core'
 import { toSignal } from '@angular/core/rxjs-interop'
+import { FormsModule } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
+import { MatTooltipModule } from '@angular/material/tooltip'
 import { ModelsService } from '@metad/cloud/state'
 import { ISemanticModelMember } from '@metad/contracts'
 import { PropertyDimension, getEntityDimensions } from '@metad/ocap-core'
 import { TranslateModule } from '@ngx-translate/core'
+import { ToastrService, tryHttp } from 'apps/cloud/src/app/@core'
+import { flatten } from 'lodash-es'
 import { derivedAsync } from 'ngxtension/derived-async'
-import { combineLatest, firstValueFrom, map } from 'rxjs'
+import { combineLatest, map } from 'rxjs'
 import { SemanticModelService } from '../model.service'
 
 @Component({
   standalone: true,
-  imports: [CommonModule, TranslateModule, MatIconModule, MatExpansionModule, MatButtonModule, MatListModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatCheckboxModule
+  ],
   selector: 'pac-model-members',
   templateUrl: 'members.component.html',
   styleUrl: 'members.component.scss'
@@ -23,6 +38,7 @@ import { SemanticModelService } from '../model.service'
 export class ModelMembersComponent {
   readonly modelService = inject(SemanticModelService)
   readonly modelsService = inject(ModelsService)
+  readonly toastrService = inject(ToastrService)
 
   readonly cubes = toSignal(this.modelService.cubes$)
   readonly virtualCubes = toSignal(this.modelService.virtualCubes$)
@@ -36,7 +52,8 @@ export class ModelMembersComponent {
           map((entityType) => ({
             name: cube.name,
             caption: cube.caption,
-            dimensions: getEntityDimensions(entityType)
+            dimensions: getEntityDimensions(entityType),
+            selectedDims: []
           }))
         )
       )
@@ -45,26 +62,45 @@ export class ModelMembersComponent {
 
   readonly loading = signal(false)
 
-  async syncMember(cube, dimension: PropertyDimension) {
+  readonly members = signal({})
+
+  async syncMember(cube: string, dimensions: PropertyDimension[]) {
     this.loading.set(true)
-    for (const hierarchy of dimension.hierarchies) {
-      const members = await firstValueFrom(
-        this.modelService.selectHierarchyMembers(cube.name, { dimension: dimension.name, hierarchy: hierarchy.name })
-      )
-
-      console.log(members)
-
-      await firstValueFrom(
-        this.modelsService.uploadDimensionMembers(
-          this.modelService.modelSignal().id,
-          members.map((member) => ({
-            ...member,
-            entity: cube.name
-          }) as unknown as ISemanticModelMember)
+    const storeMembers = {}
+    for (const dimension of dimensions) {
+      storeMembers[dimension.name] = []
+      for (const hierarchy of dimension.hierarchies) {
+        const members = await tryHttp(
+          this.modelService.selectHierarchyMembers(cube, { dimension: dimension.name, hierarchy: hierarchy.name }),
+          this.toastrService
         )
-      )
+        if (members) {
+          storeMembers[dimension.name].push(...members)
+          console.log(members)
+        }
+      }
     }
 
+    this.members.set(storeMembers)
+
+    this.loading.set(false)
+  }
+  
+  async uploadMembers(cube: string, dimensions: string[]) {
+    this.loading.set(true)
+    await tryHttp(
+      this.modelsService.uploadDimensionMembers(
+        this.modelService.modelSignal().id,
+        flatten(dimensions.map((dimension) => this.members()[dimension] ?? [])).map(
+          (member) =>
+            ({
+              ...member,
+              entity: cube
+            } as unknown as ISemanticModelMember)
+        )
+      ),
+      this.toastrService
+    )
     this.loading.set(false)
   }
 }
