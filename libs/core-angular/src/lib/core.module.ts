f5be837c7359efabc2e9ec7c4fd7d98f53edd245
meta-d <meta.digital.cloud@gmail.com>
2024-07-13 01:47:28 +0800

    fix: cache & shortnumber

--- libs/core-angular/src/lib/core.module.ts
+++ libs/core-angular/src/lib/core.module.ts
@@ -1,46 +1,30 @@
 import { ModuleWithProviders, NgModule } from '@angular/core'
+import { NgmShortNumberPipe } from '@metad/ocap-angular/core'
 import { NgmTransformScaleDirective, ResizeObserverDirective } from './directives'
-import {
-  EntriesPipe,
-  KeysPipe,
-  PropertyPipe,
-  SafePipe,
-  ShortNumberPipe
-} from './pipes/index'
+import { EntriesPipe, KeysPipe, PropertyPipe, SafePipe } from './pipes/index'
 import { NxCoreService } from './services'
 
 /**
  * @deprecated Migrate to `@metad/ocap-angular/core`
  */
 @NgModule({
-  declarations: [
-    ShortNumberPipe,
-    KeysPipe,
-    EntriesPipe,
-    PropertyPipe,
-    SafePipe,
-  ],
-  imports: [
-    NgmTransformScaleDirective,
-    ResizeObserverDirective
-  ],
+  declarations: [KeysPipe, EntriesPipe, PropertyPipe, SafePipe],
+  imports: [NgmTransformScaleDirective, ResizeObserverDirective, NgmShortNumberPipe],
   exports: [
-    ShortNumberPipe,
     KeysPipe,
     EntriesPipe,
     PropertyPipe,
     SafePipe,
     ResizeObserverDirective,
-    NgmTransformScaleDirective
+    NgmTransformScaleDirective,
+    NgmShortNumberPipe
   ]
 })
 export class NxCoreModule {
   static forRoot(): ModuleWithProviders<NxCoreModule> {
     return {
       ngModule: NxCoreModule,
-      providers: [
-        NxCoreService,
-      ]
+      providers: [NxCoreService]
     }
   }
 }
