5e577a905db35d34007533a8e6ef59c7dbad7e9a
meta-d <meta.digital.cloud@gmail.com>
2024-07-16 11:48:21 +0800

    feat: add fewShot formula for calculated command

--- apps/cloud/src/app/features/semantic-model/model/copilot/calculation/command.ts
+++ apps/cloud/src/app/features/semantic-model/model/copilot/calculation/command.ts
@@ -7,22 +7,26 @@ import { injectAgentFewShotTemplate } from 'apps/cloud/src/app/@core/copilot'
 import { NGXLogger } from 'ngx-logger'
 import { injectCreateCalculatedMeasure, injectEditFormulaAgent } from './graph'
 
+export const COPILOT_COMMAND_FORMULA_NAME = 'formula'
+export const COPILOT_COMMAND_CALCULATED_NAME = 'calculated'
+
 export function injectCalculatedCommand() {
   const logger = inject(NGXLogger)
   const translate = inject(TranslateService)
 
+  const fewShotPrompt = injectAgentFewShotTemplate(COPILOT_COMMAND_FORMULA_NAME, { k: 1, vectorStore: null })
   const createCalculatedMeasure = injectCreateCalculatedMeasure()
 
-  const commandName = 'calculated'
-  return injectCopilotCommand(commandName, {
+  return injectCopilotCommand(COPILOT_COMMAND_CALCULATED_NAME, {
     alias: 'calc',
     description: translate.instant('PAC.MODEL.Copilot.CommandCalculatedDesc', {
-      Default: 'Descripe business logic of the calculated measure'
+      Default: 'Descripe business logic of the calculated member'
     }),
     agent: {
       type: CopilotAgentType.Graph,
       conversation: true
     },
+    fewShotPrompt,
     createGraph: createCalculatedMeasure
   })
 }
@@ -33,9 +37,8 @@ export function injectFormulaCommand(calculatedMember: Signal<CalculatedMember>)
 
   const createEditFormulaAgent = injectEditFormulaAgent(calculatedMember)
 
-  const commandName = 'formula'
-  const fewShotPrompt = injectAgentFewShotTemplate(commandName, { k: 1, vectorStore: null })
-  return injectCopilotCommand(commandName, {
+  const fewShotPrompt = injectAgentFewShotTemplate(COPILOT_COMMAND_FORMULA_NAME, { k: 1, vectorStore: null })
+  return injectCopilotCommand(COPILOT_COMMAND_FORMULA_NAME, {
     alias: 'f',
     description: translate.instant('PAC.MODEL.Copilot.CommandFormulaDesc', {
       Default: 'Descripe business logic of the formula of calculated measure'
