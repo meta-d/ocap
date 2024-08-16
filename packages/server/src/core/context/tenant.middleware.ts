171ff90a66e743dea8fd603cf3d3d8bca151bf1a
meta-d <meta.digital.cloud@gmail.com>
2024-08-16 16:38:41 +0800

    fix: langgraph web in copilot

--- packages/server/src/core/context/tenant.middleware.ts
+++ packages/server/src/core/context/tenant.middleware.ts
@@ -5,7 +5,7 @@ import { RequestContext } from './request-context'
 @Injectable()
 export class TenantDomainMiddleware implements NestMiddleware {
 	use(req: Request, res: Response, next: NextFunction) {
-		if (!RequestContext.currentTenantId()) {
+		if (!RequestContext.currentTenantId() && req.headers['origin']) {
 			const url = new URL(req.headers['origin'])
 			const host = url.hostname // 获取完整的域名，如 comp.app.mtda.cloud
 			const subdomain = host.split('.')[0] // 提取第一个子域名部分
