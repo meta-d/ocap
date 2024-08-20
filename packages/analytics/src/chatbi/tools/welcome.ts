293b50a630cddaefc7adf9e130142484412f1c7f
XTXxxxxxx <doushishengsi@gmail.com>
2024-08-20 19:40:10 +0800

    文本按钮增加缩进

--- packages/analytics/src/chatbi/tools/welcome.ts
+++ packages/analytics/src/chatbi/tools/welcome.ts
@@ -10,85 +10,106 @@ export function createWelcomeTool(context: Partial<ChatContext>) {
 		async ({ models, more }): Promise<string> => {
 			logger.debug(`[ChatBI] [Copilot Tool] [Welcome] models: ${JSON.stringify(models, null, 2)}`)
 
-			conversation.messageWithEndAction([
-				{
-					tag: 'markdown',
-					content: 'Hi, 我是 ChatBI, 我可以根据你的问题分析数据、生成图表, 猜你想问：'
-				},
-				...flatten(
-					models.map(({ modelId, cubeName, prompts }) => {
-						const chatModel = conversation.models.find(
-							(model) => model.modelId === modelId && model.entity === cubeName
-						)
-						return [
-							{
-								tag: 'markdown',
-								content: `- 关于数据集 “${chatModel.entityCaption}”, 您可能关心的问题：`
-							},
-							...prompts.map((prompt) => {
-								return {
-									tag: 'action',
-									actions: [
-										{
-											tag: 'button',
-											text: {
-												tag: 'plain_text',
-												content: prompt
-											},
-											type: 'primary_text',
-											complex_interaction: true,
-											width: 'default',
-											size: 'small',
-											value: `分析数据集 “${chatModel.entityCaption}”：` + prompt,
-										}
-									]
-								}
-							})
-						]
-					})
-				),
-				...(more?.length ? [
+			conversation.messageWithEndAction(
+				[
 					{
 						tag: 'markdown',
-						content: `- 更多数据集：`
+						content: 'Hi, 我是 ChatBI, 我可以根据你的问题分析数据、生成图表, 猜你想问：'
 					},
-					...more.map(({modelId, cubeName}) => {
-						const chatModel = conversation.models.find(
-							(model) => model.modelId === modelId && model.entity === cubeName
-						)
-
-						if (!chatModel) {
-							throw new Error(`No model found for ${modelId} and ${cubeName}`)
-						}
-
-						return {
-							tag: 'action',
-							actions: [
+					...flatten(
+						models.map(({ modelId, cubeName, prompts }) => {
+							const chatModel = conversation.models.find(
+								(model) => model.modelId === modelId && model.entity === cubeName
+							)
+							return [
 								{
-									tag: 'button',
-									text: {
-										tag: 'plain_text',
-										content: chatModel.entityCaption
-									},
-									type: 'primary_text',
-									complex_interaction: true,
-									width: 'default',
-									size: 'small',
-									value: `重新给出 welcome，将数据集 “${chatModel.entityCaption}” 排在首位`,
-								}
+									tag: 'markdown',
+									content: `- 关于数据集 “${chatModel.entityCaption}”, 您可能关心的问题：`
+								},
+								...prompts.map((prompt) => {
+									return {
+										tag: 'column_set',
+										flex_mode: 'none',
+										background_style: 'default',
+										horizontal_spacing: '0px',
+										horizontal_align: 'left',
+										columns: [
+											{
+												tag: 'column',
+												width: '23px',
+												vertical_align: 'top',
+												vertical_spacing: '28px',
+												elements: []
+											},
+											{
+												tag: 'column',
+												elements: [
+													{
+														tag: 'button',
+														text: {
+															tag: 'plain_text',
+															content: prompt
+														},
+														type: 'primary_text',
+														complex_interaction: true,
+														width: 'default',
+														size: 'small',
+														value: `分析数据集 “${chatModel.entityCaption}”：` + prompt
+													}
+												]
+											}
+										],
+										margin: '16px 0px 0px 0px'
+									}
+								})
 							]
-						}
-					})
-				] : []),
-				{
-					tag: 'markdown',
-					content: `您也可以对我说 “**结束对话**” 来结束本轮对话。`
+						})
+					),
+					...(more?.length
+						? [
+								{
+									tag: 'markdown',
+									content: `- 更多数据集：`
+								},
+								...more.map(({ modelId, cubeName }) => {
+									const chatModel = conversation.models.find(
+										(model) => model.modelId === modelId && model.entity === cubeName
+									)
+
+									if (!chatModel) {
+										throw new Error(`No model found for ${modelId} and ${cubeName}`)
+									}
+
+									return {
+										tag: 'action',
+										actions: [
+											{
+												tag: 'button',
+												text: {
+													tag: 'plain_text',
+													content: chatModel.entityCaption
+												},
+												type: 'primary_text',
+												complex_interaction: true,
+												width: 'default',
+												size: 'small',
+												value: `重新给出 welcome，将数据集 “${chatModel.entityCaption}” 排在首位`
+											}
+										]
+									}
+								})
+							]
+						: []),
+					{
+						tag: 'markdown',
+						content: `您也可以对我说 “**结束对话**” 来结束本轮对话。`
+					}
+				],
+				(action) => {
+					console.log(action)
+					conversation.ask(action.value)
 				}
-			],
-			(action) => {
-				console.log(action)
-				conversation.ask(action.value)
-			})
+			)
 
 			return 'Welcome info has sent to user, waiting for user response...'
 		},
@@ -96,19 +117,26 @@ export function createWelcomeTool(context: Partial<ChatContext>) {
 			name: 'welcome',
 			description: 'Show welcome messages',
 			schema: z.object({
-				models: z.array(
-					z.object({
-						modelId: z.string().describe('The model id'),
-						cubeName: z.string().describe('The name of cube'),
-						prompts: z.array(z.string().describe('The suggestion prompt to analysis the data model'))
-					})
-				).describe('Top 3 models'),
-				more: z.array(
-					z.object({
-						modelId: z.string(),
-						cubeName: z.string()
-					}).describe('Model cube')
-				).optional().describe('The more models')
+				models: z
+					.array(
+						z.object({
+							modelId: z.string().describe('The model id'),
+							cubeName: z.string().describe('The name of cube'),
+							prompts: z.array(z.string().describe('The suggestion prompt to analysis the data model'))
+						})
+					)
+					.describe('Top 3 models'),
+				more: z
+					.array(
+						z
+							.object({
+								modelId: z.string(),
+								cubeName: z.string()
+							})
+							.describe('Model cube')
+					)
+					.optional()
+					.describe('The more models')
 			})
 		}
 	)
